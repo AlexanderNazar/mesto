f7d35f264b5963757649f20c7a58986a7af38ee6
Alexander Nazarov <naa@oplot48.ru>
2022-03-24 23:19:14 +0300

    fix: исправлены ошибки после первого ревью

--- scripts/index.js
+++ scripts/index.js
@@ -3,22 +3,22 @@ let editButton = document.querySelector('.profile__edit-button');
 let closeButton = popupElement.querySelector('.popup__close-button');
 let nameInput = popupElement.querySelector('input[name=name]');
 let professionInput = popupElement.querySelector('input[name=profession]');
-let saveButton = popupElement.querySelector('.popup__save-button');
 let profileName = document.querySelector('.profile__name');
 let profileProfession = document.querySelector('.profile__profession');
 let formElement = popupElement.querySelector('.popup__container');
 
+// Функция открытия Попапа.
 function openPopup() {
   popupElement.classList.add('popup_opened');
-
+// Подтягиваем текст профиля со страницы в формы Попапа.
   nameInput.value = profileName.textContent;
   professionInput.value = profileProfession.textContent;
 }
-
+//Функция закрытия Попапа.
 function closePopup() {
   popupElement.classList.remove('popup_opened');
 }
-
+// Функция передачи текста из формы Попапа на страницу.
 function formSubmitHandler (evt) {
   evt.preventDefault();
 
